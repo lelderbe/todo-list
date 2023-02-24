5574cadd2c204043ce6bf60bedd73202009dba7f
lelderbe <fvdc@ya.ru>
2023-02-24 13:13:53 +0300

    feat: add 'done' task property, can done tasks now

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,56 @@
 const tasks = [];
 
-const addForm = document.querySelector('.form');
+const form = document.querySelector('.form');
 // const addButton = document.querySelector('.form__button');
 const inputField = document.querySelector('.form__input');
 const list = document.querySelector('.list');
 
-addForm.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  tasks.push(inputField.value);
-  addForm.reset();
+  const title = inputField.value.trim();
+  if (!title) {
+    form.reset();
+    return;
+  }
+
+  const task = { title: title, done: false };
+  tasks.push(task);
+  form.reset();
   displayTasks();
 });
 
 function displayTasks() {
-  list.innerHTML = tasks.map((item) => {
-    return `<li class="list__item">${item}</li>`;
+  list.innerHTML = tasks.map((item, index) => {
+    const checked = item.done;
+    // const classList = checked ? 'list__item list__item--checked' :  'list__item';
+    const classList = 'list__item';
+
+    return `
+      <li class="${classList}">
+        <input class="list__checkbox" type="checkbox" ${checked ? 'checked' : ''} onclick="setTaskDone(${index})">
+        <span>
+          ${item.title}
+        </span>
+      </li>
+    `;
   }).join('');
 }
 
+function setTaskDone(id) {
+  tasks[id].done = !tasks[id].done;
+  displayTasks();
+}
+
+function initMockData() {
+  const data = [
+    { title: 'Сделать что-то хорошее', done: false },
+    { title: 'Прочитать все книги мира', done: false },
+    { title: 'Продумать план по захвату мира', done: false },
+    { title: 'Купить наркотики', done: true },
+    { title: 'Спланировать, организовать и сделать кое-что о чём нельзя говорить 🤫', done: false },
+  ]
+  tasks.push(...data);
+}
+
+initMockData();
 displayTasks();
