22a491c8e8838f5f67b96be8fee4cd5619f0e59f
lelderbe <fvdc@ya.ru>
2023-02-24 19:01:54 +0300

    feat: add trash icon, implement delete task operation

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 const tasks = [];
 
+function initMockData() {
+  const data = [
+    { title: 'Сделать что-то хорошее', done: false },
+    { title: 'Прочитать все книги мира', done: false },
+    { title: 'Продумать план по захвату мира', done: false },
+    { title: 'Купить наркотики', done: true },
+    { title: 'Спланировать, организовать и сделать кое-что, о чём нельзя говорить 🤫', done: false },
+  ]
+  tasks.push(...data);
+}
+
+initMockData();
+
 const form = document.querySelector('.form');
 // const addButton = document.querySelector('.form__button');
 const inputField = document.querySelector('.form__input');
@@ -21,36 +34,30 @@ form.addEventListener('submit', (e) => {
 
 function displayTasks() {
   list.innerHTML = tasks.map((item, index) => {
-    const checked = item.done;
-    // const classList = checked ? 'list__item list__item--checked' :  'list__item';
+    const checked = item.done ? 'checked' : '';
     const classList = 'list__item';
-
     return `
       <li class="${classList}">
-        <input class="list__checkbox" type="checkbox" ${checked ? 'checked' : ''} onclick="setTaskDone(${index})">
-        <span>
-          ${item.title}
-        </span>
+        <label class="list__label">
+          <input class="list__checkbox" type="checkbox" ${checked} onclick="toggleTaskDone(${index})">
+          <span>
+            ${item.title}
+          </span>
+        </label>
+        <i class="trash fas fa-trash" onclick="deleteTask(${index})"></i>
       </li>
     `;
   }).join('');
 }
 
-function setTaskDone(id) {
+function toggleTaskDone(id) {
   tasks[id].done = !tasks[id].done;
   displayTasks();
 }
 
-function initMockData() {
-  const data = [
-    { title: 'Сделать что-то хорошее', done: false },
-    { title: 'Прочитать все книги мира', done: false },
-    { title: 'Продумать план по захвату мира', done: false },
-    { title: 'Купить наркотики', done: true },
-    { title: 'Спланировать, организовать и сделать кое-что о чём нельзя говорить 🤫', done: false },
-  ]
-  tasks.push(...data);
+function deleteTask(id) {
+  tasks.splice(id, 1);
+  displayTasks();
 }
 
-initMockData();
 displayTasks();
