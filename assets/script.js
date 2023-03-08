093f3eca1aef18efedb649dcb62888c8c350266f
lelderbe <fvdc@ya.ru>
2023-03-08 10:06:23 +0300

    feat: add deadline field

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,28 @@ const TaskType = {
 };
 
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [
-  { title: 'Сделать что-то хорошее', done: false, description: '', type: TaskType.work.id },
-  { title: 'Прочитать все книги мира', done: false, description: 'К концу этого года', type: TaskType.private.id },
+  { title: 'Сделать что-то хорошее', done: false, description: '', type: TaskType.work.id, deadline: '2023-03-09' },
+  {
+    title: 'Прочитать все книги мира',
+    done: false,
+    description: 'К концу этого года',
+    type: TaskType.private.id,
+    deadline: '2023-03-09',
+  },
   {
     title: 'Продумать план по захвату мира',
     done: false,
     description: 'Простой и эффективный',
     type: TaskType.work.id,
+    deadline: '2023-03-09',
   },
-  { title: 'Купить наркотики', done: true, description: '', type: TaskType.private.id },
+  { title: 'Купить наркотики', done: true, description: '', type: TaskType.private.id, deadline: '2023-03-09' },
   {
     title: 'Спланировать, организовать и сделать кое-что, о чём нельзя говорить 🤫',
     done: false,
     description: '',
     type: TaskType.private.id,
+    deadline: '2023-03-09',
   },
 ];
 
@@ -31,12 +39,21 @@ const list = document.querySelector('.list');
 const chevronButton = document.querySelector('.form__chevron');
 const formDetails = document.querySelector('#form__details');
 
-// const tomorrow = new Date();
-// tomorrow.setDate(tomorrow.getDate() + 1);
-// dateField.valueAsDate = tomorrow;
+dateField.valueAsDate = getTomorrow();
 
 let formExpanded = false;
 let editTask = null;
+
+// function initForm() {
+
+// }
+
+// TODO: move to utils?
+function getTomorrow() {
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  return tomorrow;
+}
 
 function expandDetails(expand) {
   submitButton.textContent = editTask ? 'Сохранить' : 'Добавить задачу';
@@ -72,6 +89,7 @@ form.addEventListener('submit', (e) => {
   const title = titleField.value.trim();
   const description = descriptionField.value.trim();
   const type = typeField.value;
+  const deadline = dateField.value;
 
   if (!title || !TaskType[type]) {
     return;
@@ -81,13 +99,15 @@ form.addEventListener('submit', (e) => {
     editTask.title = title;
     editTask.description = description;
     editTask.type = type;
+    editTask.deadline = deadline;
     editTask = null;
   } else {
-    const task = { title: title, done: false, description: description, type: type };
+    const task = { title: title, done: false, description: description, type: type, deadline: deadline };
     tasks.push(task);
   }
 
   form.reset();
+  dateField.valueAsDate = getTomorrow();
   expandDetails(false);
   displayTasks();
 });
@@ -99,6 +119,7 @@ function displayTasks() {
       const checked = item.done ? 'checked' : '';
       const descriptionClassList = 'task__description' + (item.done ? ' task--done' : '');
       const type = TaskType[item.type]?.title || '';
+      const deadline = item.deadline ? new Date(item.deadline).toLocaleDateString('ru-RU') : '';
       return `
         <li class="list__item task">
           <div class="task__title">
@@ -109,6 +130,9 @@ function displayTasks() {
               </span>
               <div class="task__tag task-type">
                 ${type}
+              </div>
+              <div class="task__tag task-type">
+                ${deadline}
               </div>
             </label>
             <i class="edit fas fa-pen" onclick="changeTask(${index})"></i>
@@ -135,6 +159,7 @@ function changeTask(id) {
   titleField.value = editTask.title;
   descriptionField.value = editTask.description;
   typeField.value = editTask.type;
+  dateField.value = editTask.deadline;
 }
 
 function deleteTask(id) {
